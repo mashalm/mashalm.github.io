88f9b70e0953fa97378f6a7b3f7af5ee7b895caa
mashalm <mashal.mannan@gmail.com>
2015-07-01 20:08:42 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+<title>Test</title>
+
+<!-- CSS  -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+<link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+
+    <div class="row">
+        <div class="col s3">
+            <div class="container">
+                <div class="section">
+                    <div class="row">
+                        <br><Br><Br><BR><BR>
+                        <h1 class="header center pink-text text-lighten-2">
+                            Hi there
+                        </h1>
+                    </div>
+
+                    <div class="row">
+                        <h5 class="center grey-text darken-4">There's nothing here </h5>
+
+                        <p class="light center">
+                            Thanks for visiting! This page is being remade using <a href="http://materializecss.com/">Materialize CSS</a>. Come back soon.
+                        </p>
+                    </div>
+                        
+                 </div>
+            </div>
+
+        </div>
+<!--            </div>-->
+<!--    </div>-->
+        
+        <div class="col s9">
+            <div class="container">
+                <a href="https://www.flickr.com/photos/pslee999/16075990033" title="Cherry Blossom by 白士 李, on Flickr"><img src="images/cherryblossombg1.jpg" alt="Cherry Blossom" class="circle responsive-img"></a>
+<BR><BR><BR><BR><BR><BR>
+            </div>
+        </div>
+    </div>
+
+
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+<script src="../../dist/js/materialize.js"></script>
+    <script src="js/init.js"></script>
+
+</body>
+</html>
